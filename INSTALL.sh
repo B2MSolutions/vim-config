b05039089c481677a4ea4fdb7109ff8255a4fe43
Alberto Zaccagni <alberto.zaccagni@lazywithclass.com>
2014-02-25 00:19:29 +0000

    Udated comment; linked .editorconfig to ~/.vim so that proper indentation is taken into account

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,10 +4,11 @@
 # prepare the default dir
 mkdir -p ~/.vim/bundle
 
-# link ./vimrc into ~, taking care to backup an existing configuration
+# link config files, backup an existing ~/.vimrc
 mv ~/.vimrc ~/.vimrc-before-b2m 2>/dev/null
 ln -s $PWD/vimrc ~/.vim/vimrc
 ln -s $PWD/colors ~/.vim/colors
+ln -s $PWD/.editorconfig ~/.vim/.editorconfig
 
 # install the plugin manager
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
